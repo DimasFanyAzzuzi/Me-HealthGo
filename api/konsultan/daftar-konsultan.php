4a84a2377c30ada642c316b341377be76318bab7
Moh. Bahrul 'Ulum <belajarkoding97@gmail.com>
2023-01-22 11:16:37 +0700

    api konsultan

---
+++ api/konsultan/daftar-konsultan.php
@@ -0,0 +1,26 @@
+<?php
+
+include '../../pages/examples/koneksi.php';
+
+// $sql    = "SELECT * FROM artikel";
+
+$sql = "SELECT * FROM konsultan";
+
+$result = $koneksi->query($sql);
+
+if ($result->num_rows > 0) {
+    $artikel = array();
+    while ($row = $result->fetch_assoc()) {
+        $artikel[] = $row;
+    }
+    echo json_encode(array(
+        'message'   => 'Berhasil',
+        'success'   => true,
+        'data'      => $artikel 
+    ));
+} else {
+    echo json_encode(array(
+        'message'   => 'Gagal!',
+        'success'   => false
+    ));
+}
